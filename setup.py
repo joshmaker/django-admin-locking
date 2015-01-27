1e034092230390ca9054c136a24c73860981cead
Josh West <joshwest@theatlantic.com>
2015-01-27 12:20:44 -0500

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='adminlocking',
+    version='0.9',
+    url='https://github.com/joshmaker/django-admin-locking/',
+    license='BSD',
+    description='Prevents users from overwriting each others changes in Django.',
+    author='Josh West',
+    packages=find_packages(),
+    install_requires=[],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
