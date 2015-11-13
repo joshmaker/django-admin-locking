2b5f0f99d7865380824dce3279bcf25e93b0bc79
Josh West <joshwest@theatlantic.com>
2015-11-13 17:10:01 -0500

    Create initial migration for locking app
    Migrations are currently incompatible with test only models

---
+++ locking/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('contenttypes', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Lock',
+            fields=[
+                ('id', models.CharField(max_length=15, serialize=False, primary_key=True)),
+                ('date_expires', models.DateTimeField()),
+                ('object_id', models.PositiveIntegerField()),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+                ('locked_by', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'permissions': (('can_unlock', "Can remove other user's locks"),),
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AlterUniqueTogether(
+            name='lock',
+            unique_together=set([('content_type', 'object_id')]),
+        ),
+    ]
--- locking/tests/models.py
+++ locking/tests/models.py
@@ -1,12 +1,25 @@
 from __future__ import absolute_import, unicode_literals, division
 
+import sys
+
 from django import forms
+from django.conf import settings
 from django.contrib import admin
 from django.db import models
 
 from ..admin import LockingAdminMixin
 
 __all__ = ('BlogArticle', 'BlogArticleAdmin')
+
+# Test only models currently incompatible with Django migrations
+if 'test' in sys.argv or 'runtests.py' in sys.argv:
+    class DisableMigrations(object):
+        def __contains__(self, item):
+            return True
+
+        def __getitem__(self, item):
+            return "notmigrations"
+    settings.MIGRATION_MODULES = DisableMigrations()
 
 
 class BlogArticle(models.Model):
