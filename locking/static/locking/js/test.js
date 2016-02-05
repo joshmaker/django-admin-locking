f9654388698f735286ef834f0ad1657346a832f4
Josh West <joshwest@theatlantic.com>
2016-02-05 16:48:24 -0500

    Cleaned js/test.js to appease JS linter

--- locking/static/locking/js/test.js
+++ locking/static/locking/js/test.js
@@ -7,18 +7,18 @@
         },
         confirmations: 0,
         alerts: 0
-    }
+    };
     window.onerror = function(msg) {
-        locking_test.onError(msg);
-    }
+        window.locking_test.onError(msg);
+    };
     /* PhantomJS Doesn't have good support for confirming modal windows */
     window.confirm = function() {
         window.locking_test.confirmations++;
         return true;
-    }
+    };
     window.alert = function() {
         window.locking_test.alerts++;
         return true;
-    }
+    };
 
 })(window);
