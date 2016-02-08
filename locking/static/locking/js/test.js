4b602b908c547daa46f2e4ea36b90abf58a29fc8
Josh West <joshwest@theatlantic.com>
2016-02-08 10:48:37 -0500

    Added Pollyfill to get tests to pass with PhantomJS < 2.0

--- locking/static/locking/js/test.js
+++ locking/static/locking/js/test.js
@@ -21,4 +21,36 @@
         return true;
     };
 
+    /**
+     * Polyfill for `Function.prototype.bind` missing from PhantomJS < 2.0
+     * (Currently needed for tests to pass on Travis-CI)
+     */
+    if (!Function.prototype.bind) {
+      Function.prototype.bind = function(oThis) {
+        if (typeof this !== 'function') {
+          // closest thing possible to the ECMAScript 5
+          // internal IsCallable function
+          throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+        }
+
+        var aArgs   = Array.prototype.slice.call(arguments, 1),
+            fToBind = this,
+            fNOP    = function() {},
+            fBound  = function() {
+              return fToBind.apply(this instanceof fNOP
+                     ? this
+                     : oThis,
+                     aArgs.concat(Array.prototype.slice.call(arguments)));
+            };
+
+        if (this.prototype) {
+          // Function.prototype don't have a prototype property
+          fNOP.prototype = this.prototype; 
+        }
+        fBound.prototype = new fNOP();
+
+        return fBound;
+      };
+    }
+
 })(window);
